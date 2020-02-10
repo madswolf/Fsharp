91f1f2ceb537ad57ccdade79e5835b3c2ecb534a
Anna Pedersen <annpe@itu.dk>
2020-02-10 10:00:31 +0100

    ass of string

---
+++ Assignment03/Assignment3_1.fs
@@ -0,0 +1,15 @@
+﻿module Ass3_1
+
+    (* Exercise 3.1 *)
+
+    type aExp =
+        | N of int
+        | Add of aExp * aExp
+        | Sub of aExp * aExp
+        | Mul of aExp * aExp
+
+    let (.+.) a b = Add (a, b)
+    let (.-.) a b = Sub (a, b)
+    let (.*.) a b = Mul (a, b)
+
+    let rec arithEvalSimple aExp = failwith "not implemented"

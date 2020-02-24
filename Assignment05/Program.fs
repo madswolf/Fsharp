121042f5975ecc124f53b8fd0b4280c9371bc4e2
Madswolf <mads@madswolf.dk>
2020-02-24 12:42:08 +0100

    first 3

--- Assignment05/Program.fs
+++ Assignment05/Program.fs
@@ -2,6 +2,29 @@
 
 open System
 
+let sum (m:int) (n:int) : int =
+    let rec sumA (innerN:int) (acc:int) :int =
+        match innerN with 
+        |0 -> acc
+        |innerN -> sumA (innerN-1) (innerN + m + acc)
+    sumA n 0
+
+let length (list: 'a list) : int =
+    let rec lengthA (list: 'a list) (acc:int) :int =
+        match list with
+        |[] -> acc
+        |x::xs -> lengthA xs (acc + 1) 
+    lengthA list 0
+
+let foldBack (folder: 'a -> 'b -> 'b ) (list: 'a list) (acc:'b) :'b =
+    let rec foldBackC (innerList: 'a list) (continuation: 'b -> 'b): 'b =
+        match innerList with
+        | [] -> continuation acc
+        | x::xs -> foldBackC xs (fun innerAcc -> continuation(folder x innerAcc)) 
+    foldBackC list (fun x -> x)
+
+
+
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
